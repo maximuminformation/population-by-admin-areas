9924e02a617bcc41841a760ac390e3497a1b610d
giacomo-maxinfo <giacomo@maxinfo.io>
2024-09-25 00:01:59 +0100

    new filters, added shapefile output

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ This Python attributes gridded population data to polygon amministrative areas.
       format: .tif
       link: https://hub.worldpop.org/geodata/summary?id=24777
       citation: WorldPop (www.worldpop.org - School of Geography and Environmental Science, University of Southampton; Department of Geography and Geosciences, University of Louisville; Departement de Geographie, Universite de Namur) and Center for International Earth Science Information Network (CIESIN), Columbia University (2018). Global High Resolution Population Denominators Project - Funded by The Bill and Melinda Gates Foundation (OPP1134076). https://dx.doi.org/10.5258/SOTON/WP00647
-      license: 
+      license: © 2024 WorldPop datasets are licensed under the Creative Commons Attribution 4.0 International https://creativecommons.org/licenses/by/4.0/
 
 ### Administrative areas data
     - GDAM v4.1
@@ -22,4 +22,4 @@ The code accepts two inputs, the file paths of a single band tif file containing
 
 The .gpkg contains 6 layers for 6 different administrative levels, however the code will only process ```ADM_1``` and ```ADM_2``` levels.
 
-The code will iterate through each polygon for each amministrative area in each layer and calculate the total population folling within from the ```.tif``` file and save the results in a new geopackage file.
+The code will iterate through each polygon for each amministrative area in each layer and calculate the total population falling within the ```.tif``` file and save the results in a new geopackage and shape files.
--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ import rasterio.mask
 import fiona
 from shapely.geometry import MultiPolygon
 import math
+from constants import countries, NAME_1_exclusion
 
 # function to calculate the sum of all pixels (negative values counted as zero)
 def sum_pixels(tiff_file, geometry):
@@ -23,34 +24,42 @@ geopackage_path = 'data\gadm_410-levels\gadm_410-levels.gpkg'
 tiff_file = 'data\ppp_2020_1km_Aggregated.tif'
 
 # create a new GeoPackage to save the results
-output_gpkg = 'data\output_data\gadm_ppp_2020_1km_Aggregated.gpkg'
+output_gpkg = 'data\output_data\gadm_ppp_2020_1km_Aggregated_light'
 
 # list all layers in the GeoPackage
-layers = fiona.listlayers(geopackage_path )
+layers = fiona.listlayers(geopackage_path)
 
-# exclude layers named ADM_0 and ADM_1
+# keep only layers named ADM_1 and ADM_2
 layers_to_process = [layer for layer in layers if layer not in ['ADM_0', 'ADM_3', 'ADM_4', 'ADM_5']]
 
 # iterate over each layer in the geopackage
 for layer in layers_to_process:
     layer_gdf = gpd.read_file(geopackage_path, layer=layer)
+    # filter by country in the list
+    filtered_layer_gdf_country = layer_gdf[layer_gdf['GID_0'].isin(countries)].copy()
+    # filter by area name not in the list
+    filtered_layer_gdf_name_1 = filtered_layer_gdf_country[filtered_layer_gdf_country['NAME_1'].isin(NAME_1_exclusion) == False].copy()
     pixel_sums = []
-    for idx, row in layer_gdf.iterrows():
+    for idx, row in filtered_layer_gdf_name_1.iterrows():
         geometry = row['geometry']
         if isinstance(geometry, MultiPolygon):
             # if the geometry is a MultiPolygon, sum pixels for each Polygon
             total_sum = 0
             for polygon in geometry.geoms:
                 total_sum += sum_pixels(tiff_file, polygon)
-            # print(total_sum)
+            print(total_sum)
             pixel_sums.append(total_sum)
         else:
             pixel_sum = sum_pixels(tiff_file, geometry)
-            # print(pixel_sum)
+            print(pixel_sum)
             pixel_sums.append(pixel_sum)
     # add the pixel sums to the geodataframe
-    layer_gdf['pixel_sum'] = pixel_sums
-    # save the updated layer to the new geopackage
-    layer_gdf.to_file(output_gpkg, layer=layer, driver="GPKG")
+    filtered_layer_gdf_name_1['POPULATION'] = pixel_sums
+    # save the updated layer to a new geopackage
+    out_file = output_gpkg + ".gpkg"
+    filtered_layer_gdf_name_1.to_file(out_file, layer=layer, driver="GPKG")
+    # save the updated layer to a new shapefile
+    out_file = output_gpkg + f"_{layer}.shp"
+    filtered_layer_gdf_name_1.to_file(out_file, layer=layer)
 
 print(f"Results saved to {output_gpkg}")
